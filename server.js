e7f1da8b69958e7f7aeb8b5891d8dd1317d96ade
tassos <tassos@192>
2017-07-18 17:43:55 +0300

    WebSocket communication between webserver and web clients established. First tests with the web audio api.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const osc = require('osc');
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({port: 8080});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,8 +18,25 @@ app.get('/conductor', (req, res) => {
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
 
+// websockets: web server - web clients
+wss.on('connection', ws => {
+    ws.on('message', msg => {
+	console.log('Client message: ', msg);
+    });
+});
 
-//
+wss.broadcast = data => {
+    console.log('Inside wss.broadcast');
+
+    wss.clients.forEach( client => {
+	if (client.readyState === WebSocket.OPEN) {
+	    console.log('\tInside wss.broadcast. Sending data to clients');
+	    client.send(data)
+	};
+    });
+};
+
+// OSC: web server - SC
 const udpPromise = new Promise( (resolve, reject) => {
     const udpPort = new osc.UDPPort({
     // This is the port we're listening on.
@@ -30,12 +50,6 @@ const udpPromise = new Promise( (resolve, reject) => {
 });
 
     udpPort.open();
-    // udpPort.on('ready', () => {
-    // 	udpPort.on('message', (msg) => {
-    // 	    console.log(msg);
-    // 	});
-    // });
-
     udpPort.on('ready', resolve(udpPort));
     udpPort.on('error', reject(udpPort));
 });
@@ -44,7 +58,9 @@ const udpPromise = new Promise( (resolve, reject) => {
 udpPromise.then( (udpPort) => {
     return new Promise( (resolve, reject) => {
 	udpPort.on('message', (msg) => {
-	    console.log('Recieved message:\n', msg, `\n\taddress:\t ${msg.address}\n`, `\targs:\t${msg.args[0].value}, ${msg.args[1].value}, ${msg.args[2].value}`);
+	    console.log('Recieved SC message:\n', msg, `\n\taddress:\t ${msg.address}\n`, `\targs:\t${msg.args[0].value}, ${msg.args[1].value}, ${msg.args[2].value}`);
+
+	    wss.broadcast( JSON.stringify({type: msg.address, args: msg.args.map(x => x.value)}) );
 	});
     });
 }, (udpPort) => {
@@ -52,19 +68,19 @@ udpPromise.then( (udpPort) => {
 });
 
 
-udpPromise.then( (udpPort) => {
-    // OSC messages sent by web server to SC
-    const oscMsgs = {
-	start: { address: '/start' },
-	end: { address: '/end' }
-    };
+// udpPromise.then( (udpPort) => {
+//     // OSC messages sent by web server to SC
+//     const oscMsgs = {
+// 	start: { address: '/start' },
+// 	end: { address: '/end' }
+//     };
 
-    // Every second, send an OSC message to SuperCollider
-    setInterval(function() {
-	var msg = Math.random() > 0.5 ? oscMsgs.start : oscMsgs.end;
+//     // Every second, send an OSC message to SuperCollider
+//     setInterval(function() {
+// 	var msg = Math.random() > 0.5 ? oscMsgs.start : oscMsgs.end;
 
-	console.log("Sending message", msg.address, msg.args, "to", udpPort.options.remoteAddress + ":" + udpPort.options.remotePort);
-	udpPort.send(msg);
-    }, 10000);
+// 	console.log("Sending message", msg.address, msg.args, "to", udpPort.options.remoteAddress + ":" + udpPort.options.remotePort);
+// 	udpPort.send(msg);
+//     }, 10000);
 
-});
+// });
