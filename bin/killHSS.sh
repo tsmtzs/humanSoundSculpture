abc1ca82fa3f0a3985bfb2fe65c8619f39cfba7d
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2019-09-08 12:21:18 +0300

    RPi3 version

--- bin/killHSS.sh
+++ bin/killHSS.sh
@@ -2,6 +2,8 @@
 # HSS_DIR is an environment variable set at file ./startHSS.sh
 # It holds the working directory of the piece
 
+echo "Human Directory is $HSS_DIR"
+
 # Kill processes sclang and node
 killall node;
 killall sclang;
@@ -11,5 +13,5 @@ killall sclang;
 sh ${HSS_DIR}/bin/writeParNames.sh $HSS_DIR ${HSS_DIR}/bin/commonParameters
 
 # Shutdown computer after one minute
-# shutdown +1;
-
+echo raspberry | sudo -S shutdown +1;
+echo "PC Shutdown"
