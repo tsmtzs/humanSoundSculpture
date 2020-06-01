45c01735936ca56d344290cb54dfe14b20fa84f9
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2020-06-01 20:21:03 +0300

    web client: A new module 'public/sound.mjs' added
    Module 'sound.mjs' defines the object 'Sound'. This object defines a synth for
    the environment of the web client.
    
    'public/hss.js' refactored making use of the 'Sound' object.

--- public/hss.js
+++ public/hss.js
@@ -4,9 +4,10 @@
 // Web client javascript.
 // ////////////////////////////////////////////////////////////
 import { Maybe } from './functors.mjs';
+import Sound from './sound.mjs';
 
 // tap combinator
-const tap = func => a => (func(a), a);
+// const tap = func => a => (func(a), a);
 
 const addEventListener = eventType => htmlElement => listener =>  {
     htmlElement.addEventListener(eventType, listener);
@@ -25,95 +26,53 @@ const shutdownBtnListener = socket => shutdownButton => event => {
     // Change the value of the button.
     shutdownBtn.value = 'goodbye HSS';
 };
+// Play sound on clicking the 'Test' button
+const createTestSynth = soundObj => {
+    // A random frequency for each client. 
+    const freq = 400.0 + Math.random() * 600;
+    // Max duration of sound.
+    const dur = 20;
+    // Check sound in hight volume. Adjust device volume.
+    const amp = 0.9;
+    let synth;
+    
+    return () => {
+	if (synth) {
+    	    synth.stop(0);
+    	    synth = null;
+	} else {
+    	    synth = soundObj.play(freq, amp, dur);
+    	    synth.addEventListener('ended', event => synth = null);
+	}	
+    };
+};
+// WebSockets listener
+const wsListener = msgHandlerObj => message => {
+    const msg = JSON.parse(message.data);
+    console.log(msg);
+    msgHandlerObj[msg.type](...msg.args);
+
+    console.log('Websocket message: ', msg.args, msg.type, msg);
+    
+};
 
 // ////////////////////////////////////////////////////////////
 // Websockets
 // ////////////////////////////////////////////////////////////
 // Initialize WebSockets
-// '192.168.10.2' and '8080' are
+// 'hss_ip' and 'websocket_port' are
 // bash environment variables.
 // For each session they are set in server.js with a 'sed' command.
 // After perfomance, they are unset in bin/setEnvirParNames.sh'
-// from the hss-webServer@.service.
+// when the hss-webServer.service stops.
 const socket = new WebSocket('ws://192.168.10.2:8080');
 
-// ////////////////////////////////////////////////////////////
-// Sound
-// ////////////////////////////////////////////////////////////
-const audioCtx = new AudioContext();
+socket.onmessage = wsListener(wsMsgHandlerObj);
 
-// custom periodic waveform
-const mag1 = Math.random() * 0.5 + 0.4, phase1 = 0.0, mag2 = Math.random() * 0.5 + 0.4, phase2 = Math.PI * Math.random(), mag3 = Math.random() * 0.5 + 0.4, phase3 = Math.PI * Math.random();
-const real = new Float32Array([0, mag1 * Math.cos(phase1), mag2 * Math.cos(phase2), mag3 * Math.cos(phase3)]);
-const imag = new Float32Array([0, mag1 * Math.sin(phase1), mag2 * Math.sin(phase2), mag3 * Math.sin(phase3)]);
-const wave = audioCtx.createPeriodicWave(real, imag);
-
-const  synth = ( (context, wave, shaperType = 'sawtooth') => {
-    return function(freq, amp, dur) {
-	const shapingFunction = context.createOscillator();
-	const inputFunction = context.createOscillator();
-	const gain = context.createGain();
-	const indexFunction = context.createGain();
-
-	shapingFunction.type = shaperType;
-
-	// amp envelope
-	asrEnvelope(0.25 * dur, 0.5 * dur, 0.25 * dur, 0.0, amp, gain, context);
-	inputFunction.frequency.value = freq;
-
-	// apply triangular shape to index function
-	asrEnvelope(0.5 * dur, 0.0,  0.5 * dur, 50, Math.random() * 800 + 700, indexFunction, context);
-	inputFunction.connect(indexFunction);
-
-	indexFunction.connect(shapingFunction.frequency);
-
-	inputFunction.setPeriodicWave(wave);
-	shapingFunction.frequency.value = 0;
-
-	shapingFunction.connect(gain);
-	gain.connect(context.destination);
-
-	inputFunction.start(0);
-	shapingFunction.start(0);
-	shapingFunction.stop(context.currentTime + dur);
-	inputFunction.stop(context.currentTime + dur);
-
-	return shapingFunction;
-    };
-})(audioCtx, wave);
-
-
-function asrEnvelope(attack = 0.5, sustain = 0.0, release = 0.5, startValue = 0.0, endValue = 1,  gainNode, context) {
-    // console.log('Inside asrEnvelope', context);
-    const now = context.currentTime;
-
-    gainNode.gain.cancelScheduledValues(0);
-    gainNode.gain.setValueAtTime(startValue, now);
-    gainNode.gain.linearRampToValueAtTime(endValue, now + attack);
-    gainNode.gain.setValueAtTime(endValue, now + attack + sustain);
-    gainNode.gain.linearRampToValueAtTime(startValue, now + attack + sustain + release);
-};
+socket.onopen = () => console.log('WebSocket open');
+socket.onerror = event => console.log('ERROR in WebSocket', event);
 
 socket.addEventListener('open', event => {
-
-    ////////////////////////////////////////////////////////////
-    // Test button
-    ////////////////////////////////////////////////////////////
-    // const testButton = document.getElementById('soundCheckBtn');
-    // // Random test frequency for each player.
-    // const testSynthFreq = 400.0 + Math.random()*600;
-    // let testSynth, testSynthMaxDur = 20, testSynthAmp = 0.9;
-
-    // testButton.addEventListener('click', event => {
-    //     if (testSynth) {
-    // 	testSynth.stop(0);
-    // 	testSynth = null;
-    //     } else {
-    // 	testSynth = synth(testSynthFreq, testSynthAmp, testSynthMaxDur);
-    // 	testSynth.addEventListener('ended', event => testSynth = null);
-    //     }
-    // });
-
     ////////////////////////////////////////////////////////////
     // Start button
     ////////////////////////////////////////////////////////////
@@ -134,27 +93,26 @@ socket.addEventListener('open', event => {
     shutdownBtnMaybe.map(addEventListener('dblclick'))
 	.ap(shutdownBtnMaybe.map(shutdownBtnListener(socket)));
 
+    // ////////////////////////////////////////////////////////////
+    // Start AudioContext and play sound.
+    // ////////////////////////////////////////////////////////////
+    const sound = new Sound();
+
     // WebSocket message handler.
     const wsMsgHandlerObj = {
-	'/note': synth,
+	'/note': sound.play.bind(sound), // Grrrr... 'this' is very annoying
 	// '/action': do something on messages of type 'start', 'stop', 'end', 'shutdown'.
 	// Do nothing for now.
 	'/action': () => {}
     };
 
-    // console.log('Inside websocketpromise');
-    socket.onmessage = message => {
-	const msg = JSON.parse(message.data);
-	console.log(msg);
-	wsMsgHandlerObj[msg.type](...msg.args);
+    ////////////////////////////////////////////////////////////
+    // Test button
+    ////////////////////////////////////////////////////////////
+    const testButton = document.getElementById('soundCheckBtn');
 
-	console.log('Websocket message: ', msg.args, msg.type, msg);
-	
-    };
-
-    socket.onopen = () => console.log('WebSocket open');
-    socket.onerror = () => console.log('ERROR in WebSocket');
-
-    // Initialize mobileConsole for posting console messages in the web page.
-    // if (mobileConsole) mobileConsole.init();
+    testButton.addEventListener('click', createTestSynth(sound));
 });
+
+// Initialize mobileConsole for posting console messages in the web page.
+// if (mobileConsole) mobileConsole.init();
