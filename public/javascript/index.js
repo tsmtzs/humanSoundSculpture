93a93997836a564117e6719394e659970c1e05cb
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2021-05-09 18:15:14 +0300

    JavaScript adheres to the Javascript Standard Style
    Changes in the formating of JavaScript files under webserver/ and public/javascript

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,18 +1,18 @@
 // ////////////////////////////////////////////////////////////
-//		Human Sound Sculpture
-//				by Tassos Tsesmetzis
+//  Human Sound Sculpture
+//    by Tassos Tsesmetzis
 //
 // Web client JavaScript for 'index.html'.
 // Register the ServiceWorker.
 // ////////////////////////////////////////////////////////////
 // from https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register
 if ('serviceWorker' in navigator) {
-    // Register a service worker hosted at the root of the
-    // site using the default scope.
-    navigator.serviceWorker
-	.register('./sw.js')
-	.then( registration => console.log('Service worker registration succeeded:', registration))
-	.catch( error => console.log('Service worker registration failed:', error))
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker
+    .register('./sw.js')
+    .then(registration => console.log('Service worker registration succeeded:', registration))
+    .catch(error => console.log('Service worker registration failed:', error))
 } else {
-    console.log('Service workers are not supported.');
+  console.log('Service workers are not supported.')
 }
--- public/sw.js
+++ public/sw.js
@@ -1,75 +1,74 @@
+/* eslint-env serviceworker */
 // ////////////////////////////////////////////////////////////
-//		Human Sound Sculpture
-//				by Tassos Tsesmetzis
+// Human Sound Sculpture
+//        by Tassos Tsesmetzis
 //
 // Web client javascript.
 // ServiceWorker.
 // ////////////////////////////////////////////////////////////
-const cacheName = 'hss-v1';
-const interlayStr = '/views';
-const validHtmlPaths = ['conductor', 'player', 'description'];
+const cacheName = 'hss-v1'
+const interlayStr = '/views'
+const validHtmlPaths = ['conductor', 'player', 'description']
 //
-const isAcceptedHtmlReq = validPaths => req => validPaths.some(elem => req.url.endsWith(elem));
+const isAcceptedHtmlReq = validPaths => req => validPaths.some(elem => req.url.endsWith(elem))
 const interlayStrToURL = interString => url => {
-    const origin = url.origin;
-    const pathname = url.pathname;
+  const origin = url.origin
+  const pathname = url.pathname
 
-    // Return a String object.
-    return origin + interString + pathname + '.html';
-};
+  // Return a String object.
+  return origin + interString + pathname + '.html'
+}
 //
-const isHtmlReq = isAcceptedHtmlReq(validHtmlPaths);
-const interlayToURL = interlayStrToURL(interlayStr);
+const isHtmlReq = isAcceptedHtmlReq(validHtmlPaths)
+const interlayToURL = interlayStrToURL(interlayStr)
 
 // adapted from
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 self.addEventListener('install', event => {
-    event.waitUntil(
-    	caches
-    	    .open(cacheName)
-    	    .then(cache => {
-    	    	return cache.addAll([
-    	    	    '/',
-		    '/conductor',
-		    '/player',
-		    '/description',
-		    '/hss.webmanifest',
-    	    	    '/styles.css',
-		    '/views/index.html',
-		    '/views/conductor.html',
-		    '/views/player.html',
-		    '/views/description.html',
-    	    	    '/javascript/index.js',
-		    '/javascript/functors.mjs',
-		    '/javascript/sound.mjs',
-		    '/javascript/hss.js',
-		    '/icons/hssIcon_192x192.png',
-		    '/icons/hssIcon_512x512.png'
-    	    	]);
-    	    })
-	    .catch(console.log)
-    );
-});
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        return cache.addAll([
+          '/',
+          '/conductor',
+          '/player',
+          '/description',
+          '/hss.webmanifest',
+          '/styles.css',
+          '/views/index.html',
+          '/views/conductor.html',
+          '/views/player.html',
+          '/views/description.html',
+          '/javascript/index.js',
+          '/javascript/functors.mjs',
+          '/javascript/sound.mjs',
+          '/javascript/hss.js',
+          '/icons/hssIcon_192x192.png',
+          '/icons/hssIcon_512x512.png'
+        ])
+      })
+      .catch(console.log)
+  )
+})
 
 self.addEventListener('fetch', event => {
-    // Check if request points to '/conductor', '/player' or '/description'.
-    // If yes, send the corresponding html file from cache.
-    const request = isHtmlReq(event.request) ? new Request(interlayToURL(new URL(event.request.url))) : event.request;
+  // Check if request points to '/conductor', '/player' or '/description'.
+  // If yes, send the corresponding html file from cache.
+  const request = isHtmlReq(event.request) ? new Request(interlayToURL(new URL(event.request.url))) : event.request
 
-    event.respondWith(
-	caches.match(request)
-	    .then(response => response || fetch(event.request))
-	    .catch(console.log)
-    );
-});
+  event.respondWith(
+    caches.match(request)
+      .then(response => response || fetch(event.request))
+      .catch(console.log)
+  )
+})
 
 // Worker ACTIVATE event
 self.addEventListener('activate', event => {
-    event.waitUntil(
-	caches.keys().then(keyList => {
-	    return Promise.all(keyList.map(key => {
-		if (key !== cacheName) return caches.delete(key);
-	    }));
-	})
-    );
-});
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.filter(key => key !== cacheName).forEach(key => caches.delete(key)))
+    })
+  )
+})
