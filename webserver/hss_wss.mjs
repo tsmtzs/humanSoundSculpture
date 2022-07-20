cb57912ca74c6444a4be571e3f58a6e0a8ed3f72
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2022-07-20 10:09:20 +0300

    Web server: Changes in class HSS_WSS - test file added
    * webserver/hss_wss.mjs:
    	- Property 'lastClient' removed.
    	- Method 'sendToRandomClient': filtering the 'clients' array deleted.
    
    * Test file test/server/hss_wss-spec.mjs added.

--- webserver/hss_wss.mjs
+++ webserver/hss_wss.mjs
@@ -1,18 +1,11 @@
 // ////////////////////////////////////////////////////////////
 // Human Sound Sculpture
-//
-// This node.js module defines the object HSS_WSS.
-// HSS_WSS requires the package 'ws'.
-// It is an extension of 'ws''s WebSocket.Server object.
-// Defines additional functionality specific to the piece.
 // ////////////////////////////////////////////////////////////
-import { WebSocketServer } from 'ws'
+import { WebSocketServer, WebSocket } from 'ws'
 
 class HSS_WSS extends WebSocketServer {
   constructor (object) {
     super(Object.assign(object, { clientTracking: true }))
-    // Last client that played a note.
-    this.lastClient = null
   }
 
   // Broadcast message to all clients.
@@ -24,19 +17,12 @@ class HSS_WSS extends WebSocketServer {
 
   // Send data (a note) to a random client.
   sendToRandomClient (data) {
-    let clients = Array.from(this.clients)
-    console.log(`There are ${clients.length} clients online.`)
-
-    // Select all clients that are different from lastClient
-    clients = clients.length < 2 ? clients : clients.filter(elem => elem !== this.lastClient)
-
+    const clients = Array.from(this.clients)
     const size = clients.length
     const client = clients[Math.floor(Math.random() * size)]
 
     if (client && client.readyState === WebSocket.OPEN) {
-      console.log('\tA note send to a random client.')
       client.send(data)
-      this.lastClient = client
     }
   }
 }
