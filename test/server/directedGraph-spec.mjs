32367e0c017ea59b48485554579744428dc22cd7
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2022-10-14 20:09:24 +0300

    Server: Files directedGraph.mjs and directedGraph-spec.mjs added

---
+++ test/server/directedGraph-spec.mjs
@@ -0,0 +1,15 @@
+/* eslint-env mocha */
+// The next line is needed for the chai related assertions
+/* eslint-disable no-unused-expressions */
+// //////////////////////////////////////////////////
+// Tests for directedGraph.mjs
+// //////////////////////////////////////////////////
+import sinon from 'sinon'
+
+import pkg from 'chai'
+const { expect } = pkg
+
+import { DirectedGraph } from '../../webserver/functions.mjs'
+
+describe('Tests for the class DirecredGraph', function () {
+})
---
+++ webserver/directedGraph.mjs
@@ -0,0 +1,41 @@
+// ////////////////////////////////////////////////////////////
+// Human Sound Sculpture
+//
+// A DirectedGraph data structure based on
+// https://algs4.cs.princeton.edu/42digraph/
+// ////////////////////////////////////////////////////////////
+class DirectedGraph {
+  #adjacencyList = []
+  #vertices = 0
+  #edges = 0
+
+  constructor (graphOrder) {
+  }
+
+  // Accepts parallel edges and self loops.
+  addEdge (startVertex, endVertex) {
+  }
+
+  #addToEdgeNo (anInteger) {
+  }
+
+  removeEdge (startVertex, endVertex) {
+  }
+
+  adj (vertex) {
+  }
+
+  get size () {
+  }
+
+  get order () {
+  }
+
+  #isValidVertex (v) {
+  }
+
+  hasEdge (startVertex, endVertex) {
+  }
+}
+
+export { DirectedGraph }
