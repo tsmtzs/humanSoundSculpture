d78fe1b8d9a79df5c4c1a278f841d58083692f4e
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2022-10-14 18:34:34 +0300

    Client: A URL instance is used to load the service worker file in webclient/javascript/index.mjs

--- webclient/javascript/index.mjs
+++ webclient/javascript/index.mjs
@@ -9,7 +9,7 @@
 // from https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('./sw.js')
+    .register(new URL('../sw.js', import.meta.url), { type: 'module' })
     .then(registration => { console.log('Service worker registration succeeded:', registration) })
     .catch(console.error)
 } else {
