0b9adbd77407dfd3978b52079c12a544ecfdb717
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2022-10-16 10:29:52 +0300

    Server worker: Constants 'notes', 'amps', 'durs', 'adjacencyList', 'graph' and variables 'ampMultiplier', 'durMultiplier', 'deltaMultiplier' added to webserver/randomWalkOnGraph.mjs

--- webserver/randomWalkOnGraph.mjs
+++ webserver/randomWalkOnGraph.mjs
@@ -4,6 +4,42 @@
 // ////////////////////////////////////////////////////////////
 import { parentPort } from 'node:worker_threads'
 import { setTimeout } from 'node:timers'
+import { DirectedGraph } from './directedGraph.mjs'
+
+// 0 -> rest, 1 -> C, 2 -> C# etc.
+const notes = [0, 11, 10, 1, 12, 4, 5, 2, 3, 7, 6, 9, 8]
+const amps = [0.35, 0.45, 0.35, 0.5, 0.35, 0.75, 0.35, 1, 0.35, 0.75, 0.35, 0.5, 0.35]
+const durs = [1, 0.5, 1, 2 / 3, 1, 0.75, 1, 5 / 6, 1, 0.75, 1, 2 / 3, 1]
+
+let ampMultiplier = 1.0
+let durMultiplier = 21
+let deltaMultiplier = Math.random()
+
+// The adjacency list defines a Paley graph of order 13.
+const adjacencyList = [
+  [1, 3, 4, 9, 10, 12],
+  [0, 2, 4, 5, 10, 11],
+  [1, 3, 5, 6, 11, 12],
+  [0, 2, 4, 6, 7, 12],
+  [0, 1, 3, 5, 7, 8],
+  [1, 2, 4, 6, 8, 9],
+  [2, 3, 5, 7, 9, 10],
+  [3, 4, 6, 8, 10, 11],
+  [4, 5, 7, 9, 11, 12],
+  [0, 5, 6, 8, 10, 12],
+  [0, 1, 6, 7, 9, 11],
+  [1, 2, 7, 8, 10, 12],
+  [0, 2, 3, 8, 9, 11]
+]
+const graph = new DirectedGraph(adjacencyList.length)
+
+adjacencyList.forEach((endVertices, startVertex)  => {
+  endVertices.forEach(vertex => {
+    graph.addEdge(startVertex, vertex)
+  })
+})
+
+console.log('Graph', graph.order, graph.size, graph.adj(0))
 
 const msgListener = port => msg => {
   const msgHandler = {
