84d7f4f9464a5c116218ff2e341c0c4c1b6991cf
Tassos Tsesmetzis <ttsesmetzis@gmail.com>
2022-10-16 09:30:22 +0300

    Worker thread: constant 'msgListener' added in webserver/randomWalkOnGraph.mjs

--- webserver/randomWalkOnGraph.mjs
+++ webserver/randomWalkOnGraph.mjs
@@ -5,9 +5,22 @@
 import { parentPort } from 'node:worker_threads'
 import { setTimeout } from 'node:timers'
 
-parentPort.on('message', msg => {
-  console.log(`Worker got msg: \n\t${msg.type}`)
-})
+const msgListener = port => msg => {
+  const msgHandler = {
+    play: () => console.log('Worker got msg PLAY'),
+    stop: () => console.log('Worker got msg STOP'),
+    shutdown: () => {
+      console.log('Worker got msg SHUTDOWN')
+      port.close()
+      process.exit()
+    },
+  }
+
+  // console.log('Worker', msg.type)
+  msgHandler[msg.type]()
+}
+
+parentPort.on('message', msgListener(parentPort))
 
 const delta = 5000
 let id
